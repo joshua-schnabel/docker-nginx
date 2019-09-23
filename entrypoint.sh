4272e5f9a95db5edc4ba55e9c08e3c55f0370fdb
Joshua Schnabel <github@joshua-schnabel.de>
2019-09-24 00:23:34 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -Eeo pipefail
 
+SITESENABLED_DIR="/etc/nginx/sites-enabled/"
+
 ### Get nginx version
 # https://stackoverflow.com/questions/50729099/bash-nginx-version-check-cut
 ###
@@ -8,6 +10,14 @@ command="nginx -v"
 nginxv=$( ${command} 2>&1 )
 nginxlocal=$(echo $nginxv | grep -o '[0-9.]*$')
 ### Get nginx version
+
+echo "Check if ${SITESENABLED_DIR} is empty"
+
+if [ -z "$(ls -A $SITESENABLED_DIR)" ]; then
+    echo "Copy default config to ${SITESENABLED_DIR}"
+else
+    echo "Keep config in ${SITESENABLED_DIR}"
+fi
 
 echo "Starting container version $IMAGE_VERSION with nginx version ${nginxlocal}..."
 echo "Run $@"
